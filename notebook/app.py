be2d05e112c49f618fc1e59c869598d3a6d3ace0
Prathamesh Watane <127343554+praxxyy14@users.noreply.github.com>
2025-03-24 13:51:25 +0000

    Notebook Complete

--- notebook/app.py
+++ notebook/app.py
@@ -1,46 +1,59 @@
-import streamlit as st
+import streamlit as st 
 import pandas as pd
 import joblib
 
-lr_model = joblib.load("notebook/model.joblib")
+# Set the webpage title
+st.set_page_config(page_title="Iris Project")
 
-def predict_species(sep_len, sep_wid, pet_len, pet_wid):
-    d = [
-        {
-            "sepal_length": sep_len,
-            "sepal_width": sep_wid,
-            "petal_length": pet_len,
-            "petal_width": pet_wid
-        }
-    ]
+# Load the model
+model = joblib.load("notebook/model.joblib")
+
+# Function to predict results
+def predict_results(model, sep_len, sep_wid, pet_len, pet_wid):
+    # Get the data in dictionary format
+    d = {
+        "sepal_length": [sep_len],
+        "sepal_width": [sep_wid],
+        "petal_length": [pet_len],
+        "petal_width": [pet_wid]
+    }
+
+    # Convert dictionary to dataframe
     xnew = pd.DataFrame(d)
-    preds = lr_model.predict(xnew)
-    probs = lr_model.predict_proba(xnew)
-    classes = lr_model.classes_
-    probs_dct = {}
+
+    # Predict the results with probability
+    preds = model.predict(xnew)
+    probs = model.predict_proba(xnew)
+
+    # Probs save as dictionary
+    classes = model.classes_
+
+    # Apply for loop and save results in dictionary format
+    prob_d = {}
     for c, p in zip(classes, probs.flatten()):
-        probs_dct[c] = float(p)
-    return preds, probs_dct
+        prob_d[c] = float(p)
 
-if __name__ == "__main__":
-    # Initialized web page
-    st.set_page_config(page_title="Iris Project", page_icon="🛠")
-    st.title("Iris End to End project")
-    st.subheader("By Utkarsh Gaikwad")
+    # Return prediction with prob
+    return preds[0], prob_d
 
-    # Take sep len, sep wid as input
-    sep_len = st.number_input("Sepal Length", min_value=0.00, step=0.01)
-    sep_wid = st.number_input("Sepal Width", min_value=0.00, step=0.01)
-    pet_len = st.number_input("Petal Length", min_value=0.00, step=0.01)
-    pet_wid = st.number_input("Petal Width", min_value=0.00, step=0.01)
+# Creating the web app interface
+st.title("Iris Prediction Project")
+st.subheader("by Prathamesh Watane")
 
-    # Add a button to predict
-    button = st.button("predict")
+# Take inputs from user
+sep_len = st.number_input("Sepal Length", min_value=0.00, step=0.01)
+sep_wid = st.number_input("Sepal Width", min_value=0.00, step=0.01)
+pet_len = st.number_input("Petal Length", min_value=0.00, step=0.01)
+pet_wid = st.number_input("Petal Width", min_value=0.00, step=0.01)
 
-    # If button is clicked
-    if button:
-        pred, prob = predict_species(sep_len, sep_wid, pet_len, pet_wid)
-        st.subheader(f"Species Predicted : {pred[0]}")
-        for s, p in prob.items():
-            st.subheader(f"{s} : Probability {p:.4f}")
-            st.progress(p)
+# Create a button for predicting results
+submit = st.button("predict", type="primary")
+
+# After pressing submit button
+if submit:
+    pred, prob = predict_results(model, sep_len, sep_wid, pet_len, pet_wid)
+    st.subheader(f"Prediction : {pred}")
+
+    for c, p in prob.items():
+        st.subheader(f"{c} : {p:.4f}")
+        st.progress(p)
